824ced1b874bdaa69591ddf58089cb30b86627ad
Artyom <arpost89@gmail.com>
2025-07-16 11:11:29 +0400

    Auto-deploy WebGL build at 2025-07-16 11:11:29

--- deploy.ps1
+++ deploy.ps1
@@ -13,8 +13,29 @@ git pull origin main
 # 2) Удаляем всё, кроме .git
 Get-ChildItem -Force | Where-Object { $_.Name -ne ".git" } | Remove-Item -Recurse -Force
 
-# 3) Копируем свежий билд
-Copy-Item "$buildDir\*" -Destination $repoDir -Recurse
+# 3) Копируем свежий билд с исключением ненужных директорий
+$excludeDirs = @('Library', 'Temp', 'Obj', '.vs', '.git', '.idea', 'Logs', 'Builds', 'UserSettings')
+
+Get-ChildItem -Path $buildDir -Recurse -Force | Where-Object {
+    $fullPath = $_.FullName
+    foreach ($exclude in $excludeDirs) {
+        if ($fullPath -like "*\$exclude\*") {
+            return $false
+        }
+    }
+    return $true
+} | ForEach-Object {
+    $targetPath = $_.FullName.Replace($buildDir, $repoDir)
+
+    if ($_.PSIsContainer) {
+        if (!(Test-Path $targetPath)) {
+            New-Item -ItemType Directory -Path $targetPath -Force | Out-Null
+        }
+    }
+    else {
+        Copy-Item $_.FullName -Destination $targetPath -Force
+    }
+}
 
 # 4) Делаем коммит и пушим
 git add .
